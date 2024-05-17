7c8a3cae5c67f0920864935437b106b9d6e9d1c4
Deen <dejimorenigbade@gmail.com>
2024-05-17 06:25:26 -0400

    Initial commits

---
+++ APP/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!, This is Muyideen App for week4 challenge'
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ starter-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Login to Azure (if not already logged in)
+# az account show 1> /dev/null || az login   Already logged in on local environment
+
+rg=tf-week4-state-rg
+sa=tfpracticestorageweek4
+container=tfpracticecontainer
+
+# Resource group
+az group create --name $rg --location eastus --tags 'Project=Clodopsweek-4' 'Env=Demo'
+
+# account
+az storage account create --resource-group $rg --name $sa --sku Standard_LRS --encryption-services blob
+
+# container
+az storage container create --name $container --account-name $sa
